06ebad854cd77143ad72d67f1920fed4235ac176
Erin <ahnys95@yahoo.com>
2023-11-16 14:05:03 -0600

    [AccountQueries] class definition for get and create

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -2,10 +2,13 @@ import os
 from psycopg_pool import ConnectionPool
 from pydantic import BaseModel
 
-pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL"))
+
+pool = ConnectionPool(conninfo=os.environ.get("infoBASE_URL"))
+
 
 class DuplicateAccountError(ValueError):
     pass
+
 
 class AccountIn(BaseModel):
     email: str
@@ -13,65 +16,71 @@ class AccountIn(BaseModel):
     firstname: str
     lastname: str
 
+
 class AccountOut(BaseModel):
     id: str
     email: str
     firstname: str
     lastname: str
 
+
 class AccountOutWithPassword(AccountOut):
     hashed_password: str
 
-class AccountQueries(Queries):
+
+class AccountQueries:
     def get(self, email: str) -> AccountOutWithPassword:
-        try:
-            with pool.connection() as conn:
-                with conn.cursor() as db:
-                    result = db.execute(
-                        """"
-                        SELECT id
-                        , username
-                        , hashed_password
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    SELECT *
                     FROM account
-                    WHERE username =%s
+                    WHERE email = %s;
                     """,
-                    [username],
-                    )
-                    record = result.fetchone()
-                    return AccountOutWithPassword(
-                        id=record[0],
-                        username=record[1],
-                        hashed_password=record[2],
-                    )
-    # def create(self, info: AccountIn, hashed_password: str) -> AccountOutWithPassword:
+                    [email],
+                )
+                try:
+                    record = None
+                    for row in cur.fetchall():
+                        record = {}
+                        for i, column in enumerate(cur.description):
+                            record[column.name] = row[i]
+                    return AccountOutWithPassword(**record)
+                except Exception:
+                    print("exception")
+                    return {
+                        "message": "Could not get accounts record for this accounts email"
+                    }
 
+    def create(
+        self, info: AccountIn, hashed_password: str
+    ) -> AccountOutWithPassword:
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+                params = [
+                    info.firstname,
+                    info.lastname,
+                    info.username,
+                    info.email,
+                    hashed_password,
+                ]
+                cur.execute(
+                    """
+                    INSERT INTO accounts (firstname, last_name, username, email,
+                    hashed_password)
+                    VALUES (%s, %s, %s, %s, %s)
+                    RETURNING id, firstname, lastname, username,email,
+                    hashed_password
+                    """,
+                    params,
+                )
 
-
-
-# From Dalonte's twoshot postgres solved:
-#class AccountQueries:
-    # def get_all_accounts_for_user(
-    #     self, owner_id: int
-    # ) -> list[AccountOut]:
-    #     with pool.connection() as conn:
-    #         with conn.cursor() as cur:
-    #             cur.execute(
-    #                 """
-    #                  SELECT *
-    #                  FROM account
-    #                  WHERE owner_id = %s;
-    #                 """,
-    #                 [owner_id],
-    #             )
-    #             try:
-    #                 results = []
-    #                 for row in cur.fetchall():
-    #                     record = {}
-    #                     for i, column in enumerate(cur.description):
-    #                         record[column.name] = row[i]
-    #                     results.append(record)
-    #                 return results
-    #             except Exception:
-    #                 return {
-    #                     "message": "Could not get account records for this owner id"
-    #                 }
+                record = None
+                row = cur.fetchone()
+                if row is not None:
+                    record = {}
+                    for i, column in enumerate(cur.description):
+                        record[column.name] = row[i]
+                print(record)
+                return AccountOutWithPassword(**record)
