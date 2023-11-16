55157ce1fa8e7f742964aed27916670c2b097936
Michael <gambant@gmail.com>
2023-11-16 13:13:02 -0600

    [Authentication]: Added FastAPI Authentication code

---
+++ api/migrations/002_accounts_migration.py
@@ -0,0 +1,19 @@
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE IF NOT EXISTS accounts (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(256) UNIQUE,
+            firstname VARCHAR(256),
+            lastname VARCHAR(256),
+            email VARCHAR(256),
+            hashed_password VARCHAR(256),
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE accounts;
+        """
+    ],
+]
