cbc43311098ea529c0f72c5ae3b04fd5ffdd5517
Erin <ahnys95@yahoo.com>
2023-11-20 11:57:56 -0600

    [accounts] queries and routers

--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -1,22 +1,22 @@
-from fastapi import(
+from fastapi import (
     Depends,
     HTTPException,
     status,
     Response,
     APIRouter,
-    Request
+    Request,
 )
-from jwtdown_fastapi.authentication import Token
-from authenticator import authenticator
-
-from pydantic import BaseModel
-
 from queries.accounts import (
     AccountIn,
     AccountOut,
     AccountQueries,
-    DuplicateAccountError
+    DuplicateAccountError,
+    Error
 )
+from jwtdown_fastapi.authentication import Token
+from authenticator import authenticator
+from pydantic import BaseModel
+from typing import Union
 
 
 class AccountForm(BaseModel):
@@ -31,7 +31,9 @@ class AccountToken(Token):
 class HttpError(BaseModel):
     detail: str
 
+
 router = APIRouter()
+
 
 @router.get("/api/protected", response_model=bool)
 async def get_protected(
@@ -39,16 +41,19 @@ async def get_protected(
 ):
     return True
 
+
 @router.get("/token", response_model=AccountToken | None)
 async def get_token(
     request: Request,
-    account: AccountOut = Depends(authenticator.try_get_current_account_data)) -> AccountToken | None:
+    account: AccountOut = Depends(authenticator.try_get_current_account_data),
+) -> AccountToken | None:
     if authenticator.cookie_name in request.cookies:
         return {
             "access_token": request.cookies[authenticator.cookie_name],
             "type": "Bearer",
             "account": account,
         }
+
 
 @router.post("/api/accounts", response_model=AccountToken | HttpError)
 async def create_account(
@@ -68,3 +73,73 @@ async def create_account(
     form = AccountForm(username=info.email, password=info.password)
     token = await authenticator.login(response, request, form, accounts)
     return AccountToken(account=account, **token.dict())
+
+
+@router.get(
+    "/api/accounts", response_model=Union[Error, List[AccountOutWithPassword]]
+)
+async def get_all_accounts(
+    accounts: AccountQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_account_data),
+):
+    return accounts.get_all_accounts()
+
+
+@router.post("/api/accounts", response_model=Union[AccountToken, HttpError])
+async def create_account(
+    info: AccountIn,
+    request: Request,
+    response: Response,
+    accounts: AccountQueries = Depends(),
+):
+    hashed_password = authenticator.get_hashed_password(info.password)
+    try:
+        account = accounts.create_account(info, hashed_password)
+    except DuplicateAccountError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Cannot create an account with those credentials",
+        )
+    form = AccountForm(username=info.email, password=info.password)
+    token = await authenticator.login(response, request, form, accounts)
+    return AccountToken(account=account, **token.dict())
+
+
+@router.get("/api/accounts/{account_id}", response_model=AccountOut)
+async def get_account_by_id(
+    account_id: int,
+    response: Response,
+    accounts: AccountQueries = Depends(),
+) -> AccountOut:
+    account = accounts.get_account_by_id(account_id)
+    if account is None:
+        response.status_code = 404
+    return account
+
+
+@router.put("/api/accounts", response_model=Union[AccountToken, HttpError])
+async def update_account(
+    info: AccountIn,
+    request: Request,
+    response: Response,
+    accounts: AccountQueries = Depends(),
+):
+    hashed_password = authenticator.get_hashed_password(info.password)
+    try:
+        account = accounts.update_account(info, hashed_password)
+    except DuplicateAccountError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Could not update account",
+        )
+    form = AccountForm(username=info.email, password=info.password)
+    token = await authenticator.login(response, request, form, accounts)
+    return AccountToken(account=account, **token.dict())
+
+
+@router.delete("/api/accounts/{account_id}", response_model=bool)
+async def delete_account(
+    account_id: int,
+    accounts: AccountQueries = Depends(),
+) -> bool:
+    return accounts.delete_account(account_id)
