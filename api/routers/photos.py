a66b33769d2b0de18f94404a07d5444a1e992c5f
Trejon McGee <trejon.mcgee@gmail.com>
2023-11-20 16:23:54 -0600

    [Photos]: Added photos router

--- api/routers/photos.py
+++ api/routers/photos.py
@@ -1,35 +1,21 @@
-# from fastapi import File, UploadFile, HTTPException, APIRouter
-# import boto3
-# from botocore.exceptions import NoCredentialsError
-
-# router = APIRouter()
-
-# # Your AWS S3 bucket name
-# BUCKET_NAME = "prestigepalate"
-# # The folder inside the bucket where you want to store the images
-# S3_FOLDER = "images/"
-
-# s3 = boto3.client("s3")
+from fastapi import FastAPI, File, UploadFile, APIRouter
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 
-# def upload_to_s3(file, bucket_name, s3_folder):
-#     try:
-#         s3.upload_fileobj(file.file, bucket_name, s3_folder + file.filename)
-#         return True
-#     except NoCredentialsError:
-#         raise HTTPException(
-#             status_code=500, detail="AWS credentials not available."
-#         )
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+router = APIRouter()
 
 
-# @router.post("/upload/")
-# async def upload_image(file: UploadFile = File(...)):
-#     success = upload_to_s3(file, BUCKET_NAME, S3_FOLDER)
-#     if success:
-#         return {"message": "Image uploaded successfully"}
-#     else:
-#         raise HTTPException(
-#             status_code=500, detail="Failed to upload image to S3"
-#         )
+@router.post("/upload")
+async def upload_file(file: UploadFile):
+    try:
+        # Replace 'your-bucket-name' with your actual S3 bucket name
+        bucket_name = "prestigepalate"
+        s3 = boto3.client("s3")
+
+        # Upload the file to S3
+        s3.upload_fileobj(file.file, bucket_name, file.filename)
+
+        return {"message": "File uploaded successfully"}
+    except NoCredentialsError:
+        return {"message": "AWS credentials not available"}
