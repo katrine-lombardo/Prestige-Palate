840e7a64b37d75f3e1f9cfd071cb4d138806e0b2
Trejon McGee <trejon.mcgee@gmail.com>
2023-11-21 15:13:49 -0600

    [Seed Data]: Added seed_data.sql file

---
+++ api/queries/seed_data.sql
@@ -0,0 +1,5 @@
+-- Insert seed data into the restaurants table
+INSERT INTO restaurants (google_place_id) VALUES
+    ('ChIJ01sth-G3BZERHLHau6WPXV8'),
+    ('ChIJTSzGu4WipBIRkznre9Zwis0'),
+    ('ChIJfwMp6PLSTw0RFlSWXV3fkio');
