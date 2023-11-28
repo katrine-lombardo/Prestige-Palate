db20b4c0091ed9b8273239a53374ce3020916dcf
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-11-28 18:31:29 -0500

    [database]: Photos migration table updated

--- api/migrations/005_photos_migration.py
+++ api/migrations/005_photos_migration.py
@@ -4,8 +4,8 @@ steps = [
         """
         CREATE TABLE photos (
             id SERIAL PRIMARY KEY,
-            user_id INT NOT NULL REFERENCES accounts(id),
-            photo_url VARCHAR(255) NOT NULL,
+            username VARCHAR(256) NOT NULL REFERENCES accounts(username),
+            photo_url VARCHAR(256) NOT NULL,
             upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             place_id VARCHAR(300) UNIQUE,
             review_id INT NOT NULL REFERENCES reviews(id)
