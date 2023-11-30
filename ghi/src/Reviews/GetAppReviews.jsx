2c3a7c77d844d8496a996b9b59ab924528879432
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-11-30 15:12:54 -0500

    [disregard]: checkout out main

--- ghi/src/Reviews/GetAppReviews.jsx
+++ ghi/src/Reviews/GetAppReviews.jsx
@@ -3,8 +3,13 @@ import React, { useEffect, useState } from "react";
 const AppReviewList = () => {
     const [reviews, setReviews] = useState([]);
 
-    const fetchData = async () => {
-        const url = `http://localhost:8000/api/accounts/${username}/reviews`;
+    const fetchRestaurants = async () => {
+        const url = `http://localhost:8000/api/restaurants/${place_id}/reviews`;
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            setReviews(data.reviews)
+        }
     }
 }
 
