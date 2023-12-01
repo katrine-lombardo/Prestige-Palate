e75aa806bfaf1cb110a3a6e4465158603173b451
Yaosheng <yinyaosheng@gmail.com>
2023-12-01 12:40:32 -0500

    add filter by state

--- ghi/src/Restaurants/RestaurantCard.jsx
+++ ghi/src/Restaurants/RestaurantCard.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const RestaurantCard = ({ restaurant }) => {
-
+    const addressParts = restaurant.formattedAddress.split(', ');
+    const city = addressParts.length > 2 ? addressParts[1] : null;
+    const state = addressParts.length > 2 ? addressParts[2].split(' ')[0] : null; 
+    const conutry = addressParts.length > 2 ? addressParts[3] : null;
     return (
         <div className="restaurant-card">
             <Link to={`/restaurants/${restaurant.id}`} className="restaurant-link">
                 <h3>{restaurant.displayName.text}</h3>
                 <p>{restaurant.formattedAddress}</p>
+                <p>{city}</p>
+                <p>{state}</p>
+                <p>{conutry}</p>
                 <p>Rating: {restaurant.rating}({restaurant.userRatingCount} rating)</p>
             </Link>
         </div>
