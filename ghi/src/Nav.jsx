7dd3e6f56609279dcc8292e4b86d5789c8dd6d12
Erin <ahnys95@yahoo.com>
2023-12-04 08:42:44 -0600

    fixed layout of nav bar

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
+import SearchResults from "./SearchResults";
 
-function Nav() {
+function Nav({ toggleSidebar }) {
+    const [restaurants, setRestaurants] = useState([]);
+
+    const handleSearchResults = (results) => {
+        setRestaurants(results);
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">
-                    Prestige Palate
-                </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <NavLink
-                                className="nav-link"
-                                activeClassName="active"
-                                to="/"
-                            >
-                                Home
-                            </NavLink>
-                        </li>
-                    </ul>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">
+                        Prestige Palate
+                    </Link>
+                    <div className="d-flex justify-content-center">
+                        <SearchBar onSearch={handleSearchResults} />
+                    </div>
+                    <div
+                        className="user-icon-container d-flex align-items-center"
+                        onClick={toggleSidebar}
+                    >
+                        <img
+                            src="https://cdn-icons-png.flaticon.com/512/9131/9131529.png"
+                            alt="User"
+                            className="user-icon"
+                        />
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+
+            <main className="homepage-content">
+                <SearchResults results={restaurants} />
+            </main>
+        </>
     );
 }
 
