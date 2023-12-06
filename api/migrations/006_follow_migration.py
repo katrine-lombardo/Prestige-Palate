e72126c8837ce3eabe65a2d50890ed8684a5ddfa
Erin <ahnys95@yahoo.com>
2023-12-06 00:40:52 -0600

    follow migration

--- api/migrations/006_follow_migration.py
+++ api/migrations/006_follow_migration.py
@@ -7,6 +7,7 @@ steps = [
             PRIMARY KEY (follower_username, following_username),
             FOREIGN KEY (follower_username) REFERENCES accounts(username),
             FOREIGN KEY (following_username) REFERENCES accounts(username)
+            CHECK (follower_username <> following_username)
         );
         """,
         """
