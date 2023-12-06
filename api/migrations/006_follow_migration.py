c8ae6cacb6f23caeb170af20a3d0307023085781
Erin <ahnys95@yahoo.com>
2023-12-06 00:46:39 -0600

    follow

--- api/migrations/006_follow_migration.py
+++ api/migrations/006_follow_migration.py
@@ -6,8 +6,8 @@ steps = [
             following_username VARCHAR(256) NOT NULL,
             PRIMARY KEY (follower_username, following_username),
             FOREIGN KEY (follower_username) REFERENCES accounts(username),
-            FOREIGN KEY (following_username) REFERENCES accounts(username)
-            CHECK (follower_username <> following_username)
+            FOREIGN KEY (following_username) REFERENCES accounts(username),
+            CONSTRAINT unique_followers CHECK (follower_username <> following_username)
         );
         """,
         """
