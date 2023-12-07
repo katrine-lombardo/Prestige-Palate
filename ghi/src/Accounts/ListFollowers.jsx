a11625fb1b007273ddfeab9b6f7012aa8c4dcd4b
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-12-07 12:37:43 -0500

    [Follows]: Names and review data connected

--- ghi/src/Accounts/ListFollowers.jsx
+++ ghi/src/Accounts/ListFollowers.jsx
@@ -9,6 +9,7 @@ if (!tokenUrl) {
 
 const ListFollowers = ({ username }) => {
     const [followers, setFollowers] = useState([]);
+    const [followerReviewData, setFollowerReviewData] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const { token } = useAuthContext();
@@ -16,27 +17,46 @@ const ListFollowers = ({ username }) => {
     useEffect(() => {
         const fetchFollowers = async () => {
             if (username) {
-                const url = `${tokenUrl}/api/accounts/followers/${username}`;
-                fetch(url, {
-                    credentials: "include",
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        setFollowers(data);
-                        console.log(data)
-                        setLoading(false);
-                    })
-                    .catch(
-                        (error) => {
-                            console.error("Fetch error: ", error);
-                            setError(error.message);
-                        }
+                try {
+                    const url = `${tokenUrl}/api/accounts/followers/${username}`;
+                    const response = await fetch(url, {
+                        credentials: "include",
+                    });
+                    const data = await response.json();
+                    console.log("data: ", data);
+
+                    const reviewsWithFollowerData = await Promise.all(
+                        data.map(async (follower_username) => {
+                            try {
+                                const reviewUrl = `${tokenUrl}/api/accounts/${follower_username}/reviews`;
+                                const reviewResponse = await fetch(reviewUrl);
+                                const followerReviewData = await reviewResponse.json();
+                                return {
+                                    follower: follower_username,
+                                    profile_icon_url: followerReviewData.profile_icon_url,
+                                };
+                            } catch (error) {
+                                console.error("Error fetching review:", error);
+                                return {
+                                    follower: follower_username,
+                                    profile_icon_url: null, // Set a default value or handle the error as needed
+                                };
+                            }
+                        })
                     );
+                    setFollowers(reviewsWithFollowerData);
+                    console.log("reviews with follower data: ", reviewsWithFollowerData)
+                    setLoading(false);
+                } catch (error) {
+                    console.error("Fetch error:", error);
+                    setError(error.message);
+                    setLoading(false);
+                }
             }
         };
         fetchFollowers();
-
     }, [token, username]);
+
 
     const renderNullFollowers = () => (
         <div>
@@ -56,7 +76,9 @@ const ListFollowers = ({ username }) => {
                                 <div className="col">
                                     <div className="card h-100">
                                         <div className="card-body">
-                                            <h5 className="card-title">{follower}</h5>
+                                            <h5 className="card-title">{follower.follower}</h5>
+                                            {/* <img src={follower.profile_icon_url} alt={follower.follower_username} /> */}
+                                            <p>{follower.profile_icon_url}</p>
                                         </div>
                                     </div>
                                 </div>
