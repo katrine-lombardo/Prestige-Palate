e1a1018a9f738326a63a9d2fd9e2924a04a4df05
Michael <gambant@gmail.com>
2023-12-08 13:12:15 -0600

    [Unit-test] Referral flake8 finished

--- api/tests/api/routers/test_referrals.py
+++ api/tests/api/routers/test_referrals.py
@@ -8,6 +8,10 @@ from authenticator import authenticator
 fake_user_data = {"email": "test@example.com", "username": "testUser"}
 
 
+def fake_user():
+    return fake_user_data
+
+
 class FakeReferralQueries:
     def refer_email(self, info: str, email: str):
         if email != "test@example.com" or info != "referred@example.com":
@@ -19,14 +23,19 @@ class FakeReferralQueries:
             return []
         return ["referred1@example.com", "referred2@example.com"]
 
+
 client = TestClient(app)
+
 
 @pytest.fixture(autouse=True)
 def setup_tests():
-    app.dependency_overrides[ReferralQueries] = lambda: FakeReferralQueries()
-    app.dependency_overrides[authenticator.get_current_account_data] = lambda: fake_user_data
+    app.dependency_overrides[ReferralQueries] = FakeReferralQueries
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_user
     yield
     app.dependency_overrides.clear()
+
 
 def test_refer_email():
 
@@ -37,4 +46,3 @@ def test_refer_email():
 def test_get_referrals_given():
     response = client.get("/api/accounts/referred_by/test@example.com")
     assert response.status_code == 200 or response.status_code == 422
-
