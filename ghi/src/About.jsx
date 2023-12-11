549ce9138db982eb9463f3d990c87ac31f827739
Trejon McGee <trejon.mcgee@gmail.com>
2023-12-11 09:15:47 -0600

    [AboutUs]: Updated my about me section

--- ghi/src/About.jsx
+++ ghi/src/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css';
 import erin from './imgs/erin.jpg'
+import trejon from './imgs/trejon.jpg'
 
 const About = () => {
     return (
@@ -29,11 +30,11 @@ const About = () => {
                                         <div className="p-4">
                                             <h6 className="mb-3 font-weight-medium">ERIN AHN</h6>
                                             <p style={{ fontSize: '11px' }}>
-                                                Hey there! As a software engineer with a unique background in speech-language pathology, I bring a fresh perspective to the world of coding. 
+                                                Hey there! As a software engineer with a unique background in speech-language pathology, I bring a fresh perspective to the world of coding.
                                                 With my experience in helping individuals communicate effectively, I've developed a knack for problem-solving and attention to detail that translates
-                                                seamlessly into my software development skills. I'm thrilled to combine my passion for technology with my understanding of language and 
+                                                seamlessly into my software development skills. I'm thrilled to combine my passion for technology with my understanding of language and
                                                 communication to create innovative solutions that bridge the gap between the two fields.
-                                                I'm excited to use my diverse background to build software that not only functions 
+                                                I'm excited to use my diverse background to build software that not only functions
                                                 flawlessly but also enhances the way we connect and communicate.
                                             </p>
                                         </div>
@@ -85,7 +86,7 @@ const About = () => {
                             <div className="card card-shadow border-0 mb-4">
                                 <div className="row no-gutters card-shadow">
                                     <div className="col-md-5">
-                                        <img src="CHANGETHIS" alt="Team Member" className="pro-pic" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                                        <img src={trejon} alt="Team Member" className="pro-pic" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
                                         <div className="card-img-overlay d-flex justify-content-end align-items-start">
                                             <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://www.linkedin.com/in/trejonmcgee/" role="button" data-mdb-ripple-color="dark">
                                                 <i className="fab fa-linkedin"></i>
@@ -94,8 +95,13 @@ const About = () => {
                                     </div>
                                     <div className="col-md-7 bg-white">
                                         <div className="p-4">
-                                            <h6 className="mb-3 font-weight-medium">TREY MCGEE</h6>
-                                            <p style={{ fontSize: '11px' }}>EDIT YOUR DESCRIPTION</p>
+                                            <h6 className="mb-3 font-weight-medium">TREJON MCGEE</h6>
+                                            <p style={{ fontSize: '11px' }}>
+                                                Hello! My name is Trejon. I'm a highly motivated software engineer with a strong background in Python, JavaScript, and React.
+                                                My technical expertise is complemented by my proficiency in efficiently solving intricate problems through a blend of creative and analytical thinking.
+                                                My inherent curiosity and passion for bringing concepts to life drive me to remain current with the latest advancements in technology.
+                                                Moving forward, I am eager to leverage my skill set in contributing to cutting-edge innovations and addressing the evolving challenges in software development.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
