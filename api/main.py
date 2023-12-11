747df4198d018b6feaaca4a49244d8e81a2e251a
Erin <ahnys95@yahoo.com>
2023-12-11 14:50:59 -0600

    public url

--- api/main.py
+++ api/main.py
@@ -20,10 +20,9 @@ app.include_router(favorites.router, tags=["Favorites"])
 CORS_HOST = os.environ.get("CORS_HOST")
 if not CORS_HOST:
     origins = [
-        "http://localhost:3000",
         "http://localhost:5173",
-        "https://mambo-number-5.gitlab.io/prestige-palate/"
-        ]
+        "https://mambo-number-5.gitlab.io/prestige-palate/",
+    ]
 else:
     origins = [CORS_HOST]
 
