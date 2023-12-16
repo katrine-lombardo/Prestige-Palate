907075721ee70dfcaf7f2d1fb66f8ea2f1e5fb11
Erin <ahnys95@yahoo.com>
2023-12-15 23:58:40 -0600

    unit test

--- api/middleware.py
+++ api/middleware.py
@@ -4,7 +4,6 @@ from fastapi.middleware.base import BaseHTTPMiddleware
 
 class TrailingSlashMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        # Modify request URL logic here, e.g., remove trailing slash
         if request.url.path.endswith("/"):
             request.url = request.url.copy_with(
                 path=request.url.path.rstrip("/")
